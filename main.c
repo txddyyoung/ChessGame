e264389f5b3abcfcedea16cf27e431b1dbfcaba9
Teddy Young <teddy@intellectstorm.com>
2023-11-19 06:08:31 -0400

    create function to initialize checkers gameBoard

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
+#define BOARD_SIZE 8
 
+// Function to initialize the checkers board
+void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE])
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            // Set up initial positions for black and white pieces
+            if ((i + j) % 2 == 0)
+            {
+                if (i < 3)
+                {
+                    board[i][j] = 'B'; // Black piece
+                }
+                else if (i > 4)
+                {
+                    board[i][j] = 'W'; // White piece
+                }
+                else
+                {
+                    board[i][j] = ' '; // Empty square
+                }
+            }
+            else
+            {
+                board[i][j] = ' '; // Empty square
+            }
+        }
+    }
+}
 
 int main()
 {
